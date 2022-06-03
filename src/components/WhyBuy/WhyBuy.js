fb1c2c31a1ffd3af9c54ba227add164f0c333468
vsvijay987 <vsvijju987@gmail.com>
2022-06-03 18:52:20 +0530

    blink effect in why buy

--- src/components/WhyBuy/WhyBuy.js
+++ src/components/WhyBuy/WhyBuy.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 const WhyBuy = () => {
+    const spring = useSpring({
+        loop: true,
+        to: [{ opacity: 1, color: "#d6b600" }],
+        from: { opacity: 0, color: "white" },
+    });
     return (
         <div
             id="featureArtist"
@@ -15,47 +21,55 @@ const WhyBuy = () => {
             <div className="">
                 <ul className="list-disc font-para px-8 space-y-6">
                     <li>
-                        <span className="text-primary">Passive Income: </span>
+                        <animated.span style={spring}>
+                            Passive Income:{" "}
+                        </animated.span>
                         Not a boring 2D picture. Real world utilities and
                         Passive income
                     </li>
                     <li>
-                        <span className="text-primary">Metaverse: </span>
+                        <animated.span style={spring}>
+                            Metaverse:{" "}
+                        </animated.span>
                         Project is a part of P2E games, multi-storyline spinoffs
                         and metaverse where they will serve as playable,
                         open-storyline characters and avatars.
                     </li>
                     <li>
                         {" "}
-                        <span className="text-primary">Eco Friendly: </span>
+                        <animated.span style={spring}>
+                            Eco Friendly:{" "}
+                        </animated.span>
                         Ensuring the lowest gas fees at the time of minting to
                         avoid wastage of electricity and natural Resources{" "}
                     </li>
                     <li>
-                        <span className="text-primary">Passive Income: </span>
+                        <animated.span style={spring}>
+                            Passive Income:{" "}
+                        </animated.span>
                         Paid out quarterly. 20% of all secondary sales will be
                         paid out to nft holders as per holding status. $1 m
                         sales (10% royalty: 100,000$) -$ 20,000 to holders
                     </li>
                     <li>
-                        <span className="text-primary">Creed: </span>We will be
-                        a tight-knit artist and an enthusiastic community, a
-                        creed of like-minded people, investing in Startup ideas
-                        of our community members. 20% of all Secondary sales
-                        will be earmarked for the same
+                        <animated.span style={spring}>Creed: </animated.span>
+                        We will be a tight-knit artist and an enthusiastic
+                        community, a creed of like-minded people, investing in
+                        Startup ideas of our community members. 20% of all
+                        Secondary sales will be earmarked for the same
                     </li>
                     <li>
-                        <span className="text-primary">Charity: </span>After
-                        round 1 sales. Up to $25k will be donated to charity to
-                        help women empowerment, child education, and
+                        <animated.span style={spring}>Charity: </animated.span>
+                        After round 1 sales. Up to $25k will be donated to
+                        charity to help women empowerment, child education, and
                         environmental protection. 25% of all secondary sales
                         will be donated to the cause collectively decided by the
                         dao. You decide the future of the world
                     </li>
                     <li>
-                        <span className="text-primary">
+                        <animated.span style={spring}>
                             Frequent Giveways:{" "}
-                        </span>{" "}
+                        </animated.span>{" "}
                         Amounting to $50k in stage 1 and $100k at the end of
                         sale completion. Airdrop on native currency for holders
                         exceeding 3 months. Preference to land sales and elite
@@ -63,10 +77,10 @@ const WhyBuy = () => {
                         decentraland and frequent giveaways.
                     </li>
                     <li>
-                        <span className="text-primary">Legacy: </span>After
-                        metaverse announcement. A global event where free work
-                        from remote locations will be announced. We are not just
-                        another project. We are here to rule the blockchain
+                        <animated.span style={spring}>Legacy: </animated.span>{" "}
+                        After metaverse announcement. A global event where free
+                        work from remote locations will be announced. We are not
+                        just another project. We are here to rule the blockchain
                         world as gods.
                     </li>
                 </ul>
